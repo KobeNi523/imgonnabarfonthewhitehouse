9b4346a81b4eff4676878a167ed455f41cd17f57
KobeNi523 <104379724+KobeNi523@users.noreply.github.com>
2026-04-22 14:02:04 -0500

    Add files via upload

--- posts.js
+++ posts.js
@@ -4,17 +4,7 @@ window.BLOG_POSTS = [
     profilePicture: "pfp.jpg",
     date: "2026-04-22T13:46:00-05:00",
     text: "So THIS Happened.",
-    photos: ["whitehouse.png"],
-    likes: 0,
-    comments: 0,
-    shares: 0
-  },
-  {
-    name: "Kobe Ni",
-    profilePicture: "pfp.jpg",
-    date: "2026-04-22T13:46:00-05:00",
-    text: "So THIS Happened.",
-    photos: ["whitehouse.png"],
+    photos: ["soTHIShappened.png"],
     likes: 0,
     comments: 0,
     shares: 0
